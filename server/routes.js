45d8f76525863436126efe47e2701d2060a77dfa
RobertoSanchez <sanchez.roberto0810@gmail.com>
2019-05-06 20:57:33 -0600

    Rutas para API

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,56 @@
+// Inicialiacion del router
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
+const bodyparser = require('body-parser')
+router.use(bodyparser())
+// Mysql
+const mysql = require("mysql2/promise")
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '37292636',
+  database: 'SISTEMA_NOMINA'
+})
+// QueryMaker
+const QueryMaker = require("./helper")
+const queryMaker = new QueryMaker()
 
 router.get("/", (req, res) => res.send('Hellou Warudo'))
+// Trabajadores
+router.get("/trabajador", async (req, res) => {
+  const [rows] = await db.query("Select * from Trabajador")
+  res.send(rows)
+})
+router.get("/trabajador/:id", async (req, res) => {
 
+  try {
+    const id = req.params.id
+    const [rows] = await db.query(queryMaker.select("*")
+      .from("Trabajador").make())
+    // Es igual a 
+    /*const [rows] = await db.query(`Select * from Trabajador where idTrabajador = ${id}`)*/
+    res.send(rows)
+
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+router.post("/trabajador", async (req, res) => {
+  const content = req.body
+  try {
+    const [rows] = await db.query(
+      queryMaker.insert("Trabajador", content)
+        .make())
+    console.log(rows)
+  }
+  catch (e) {
+    console.log(e)
+  }
+
+})
+
+// TODO: UPDATE TRABAJADOR
+// "ELIMINAR TRABAJADO"
+//
 module.exports = router
