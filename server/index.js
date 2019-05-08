db9fddf8b27f515718bf676228d842a66909da61
RobertoSanchez <sanchez.roberto0810@gmail.com>
2019-05-07 22:16:54 -0600

    router estatico agregado al server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const apiRouter = require('./ApiRouter.js')
 const app = express()
-
+const staticRouter = require("./staticRouter.js")
 // Global Middleware
 app.use(express.urlencoded({ extetended: true }))
 app.use("/api", apiRouter)
+app.use(staticRouter)
 // Servidor Escuchar
 app.listen(3000)
 
