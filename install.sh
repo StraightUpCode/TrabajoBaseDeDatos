54febaac7372a35c05dff6d6e0c9c46fe99c1a3c
RobertoSanchez <sanchez.roberto0810@gmail.com>
2019-05-09 20:32:57 -0600

    nada importante

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #! bin/bash
 #TODO: TESTEAR EL INSTALADOR
 
+
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 git clone https://github.com/StraightUpCode/TrabajoBaseDeDatos.git
 
-cat *script SQL * | mysql -u root -p"Contraseña"
+cat ./dbscript.sql | mysql -u root -p
 
 cd TrabajoBaseDeDatos
 npm run start 
