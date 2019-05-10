b472629de168a45f173a2b34e076d273685ef68c
RobertoSanchez <sanchez.roberto0810@gmail.com>
2019-05-09 20:30:46 -0600

    se agrega el router para las login y el middleware para la sesion

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const apiRouter = require('./ApiRouter.js')
 const app = express()
 const staticRouter = require("./staticRouter.js")
+const sessions = require('./AuthMiddleware/session')
+const authRouter = require('./AuthMiddleware/authRouter')
 // Global Middleware
 app.use(express.urlencoded({ extetended: true }))
+app.use(sessions)
+app.use(authRouter)
+app.use("/checkSession", (req, res) => {
+  console.log(req.session)
+})
 app.use("/api", apiRouter)
 app.use(staticRouter)
 // Servidor Escuchar
