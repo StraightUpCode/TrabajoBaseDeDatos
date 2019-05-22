b5ef4c7de1840a9f700fff456ff8d8b16d4a8e8b
luzpm1102 <50502888+luzpm1102@users.noreply.github.com>
2019-05-22 13:51:14 -0600

    Update authRouter.js

--- server/AuthMiddleware/authRouter.js
+++ server/AuthMiddleware/authRouter.js
@@ -12,9 +12,9 @@ router.post("/login", async (req, res) => {
     console.log({ username, password })
     if (username === user.username && password === user.password) {
       req.session.rol = user.rol
+      console.log(req.session)
       req.session.save()
-      
-    res.redirect("/")
+      res.redirect("/")
     }
   } catch (e) {
     console.log(e)
