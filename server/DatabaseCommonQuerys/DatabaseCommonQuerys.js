f747befe4e324eb3f779e22a09638c0e3ee3e6f6
RobertoSanchez <sanchez.roberto0810@gmail.com>
2019-05-25 20:50:03 -0600

    nuevas funciones para el api

--- server/DatabaseCommonQuerys/DatabaseCommonQuerys.js
+++ server/DatabaseCommonQuerys/DatabaseCommonQuerys.js
@@ -1,9 +1,11 @@
 const getCargos = require('./cargos')
 const getDiasPago = require('./diasDePago')
 const querysTrabajador = require('./trabajador')
+const getFrecuenciaDePago = require('./frecuenciaDePago')
 
 module.exports = {
   getCargos,
   getDiasPago,
-  ...querysTrabajador
-} 
+  ...querysTrabajador,
+  getFrecuenciaDePago
+}
