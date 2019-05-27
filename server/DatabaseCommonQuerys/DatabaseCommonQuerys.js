f7511d2bda5887dc683e08b1a9fef88a87b7d2ff
RobertoSanchez <sanchez.roberto0810@gmail.com>
2019-05-26 21:34:03 -0600

    Nuevas Funciones Disponibles para busquedas de Usuarios

--- server/DatabaseCommonQuerys/DatabaseCommonQuerys.js
+++ server/DatabaseCommonQuerys/DatabaseCommonQuerys.js
@@ -2,10 +2,11 @@ const getCargos = require('./cargos')
 const getDiasPago = require('./diasDePago')
 const querysTrabajador = require('./trabajador')
 const getFrecuenciaDePago = require('./frecuenciaDePago')
-
+const user = require('./user')
 module.exports = {
   getCargos,
   getDiasPago,
   ...querysTrabajador,
-  getFrecuenciaDePago
+  getFrecuenciaDePago,
+  ...user
 }
